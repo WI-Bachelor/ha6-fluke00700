97a6681496b7461d365e7238656985f1e9b59722
fluke00700 <teerapat.thiti@gmail.com>
2021-01-15 12:25:26 +0100

    1. Upload

--- src/main/java/htw/berlin/ui/BurgerBotCommands.java
+++ src/main/java/htw/berlin/ui/BurgerBotCommands.java
@@ -1,11 +1,18 @@
 package htw.berlin.ui;
 
 import htw.berlin.domain.Burger;
+import htw.berlin.domain.Ingredient;
 import htw.berlin.service.BurgerBuilder;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 // TODO (2) Annotations @ShellComponent, @ShellCommand, und @ShellOption verwenden
+@ShellComponent
 public class BurgerBotCommands {
 
   final List<Burger> orderedBurgers;
@@ -21,17 +28,35 @@ public class BurgerBotCommands {
     this.burgerBuilder = burgerBuilder;
     this.menu = Menu.getInstance();
   }
+  @ShellMethod("order something")
+  public String order(@ShellOption("--text")String inputLine) {
+    // DONE TODO (3) inputParser, burgerBuilder, menu, orderedBurgers nutzen, um Bestellungen aufzunehmen
+    List<Ingredient> ingredientList = inputParser.parseIngredientsFromOrder(inputLine);
+    String failedOrder = ("Entschuldigung, ich habe dich nicht verstanden. Waehle aus folgenden Zutaten: " + Menu.getInstance().printAllIngredients());
 
-  public String order(String inputLine) {
-    // TODO (3) inputParser, burgerBuilder, menu, orderedBurgers nutzen, um Bestellungen aufzunehmen
+    if(ingredientList.isEmpty())
+      return failedOrder;
+    else {
+      BurgerBuilder burger = burgerBuilder.addIngredients(ingredientList);
+      this.orderedBurgers.add(burger.build());
+      int currentBurger = orderedBurgers.size();
+      Burger currentBurgerordered = this.orderedBurgers.get(currentBurger - 1);
 
-    return "In Ordnung ...";
+      String orderSuccessful = "In Ordnung. Dein " + currentBurger + ". Burger mit " + currentBurgerordered.getIngredients().toString() + " kostet " + currentBurgerordered.calculatePrice()
+              + "EUR.\nGib <confirm> ein, " + "um die Bestellung abzuschliessen oder bestelle einen weiteren Burger mit <order -t '...'>";
+      return orderSuccessful;
+    }
     // TODO (5) Klasse BurgerBotPromptProvider erstellen und dann inputEventPublisher für Prompt-Updates nutzen
   }
 
+  @ShellMethod("confirms all orders up to now")
   public String confirm() {
-    // TODO (4) Preis der gesamten Bestellung mithilfe von orderedBurgers berechnen
+    // DONE TODO (4) Preis der gesamten Bestellung mithilfe von orderedBurgers berechnen
+      var totalPrice = new BigDecimal(0);
+      for (Burger burger : this.orderedBurgers) {
+        totalPrice = totalPrice.add(burger.calculatePrice());
+      }
+      return "Vielen Dank fuer deine Bestellung. Du hast " + this.orderedBurgers.size() + " Burger bestellt. Die Gesamtsumme betraegt " + totalPrice + " EUR.";
 
-    return "Vielen Dank...";
   }
 }
---
+++ src/main/java/htw/berlin/ui/BurgerBotPromptProvider.java
@@ -0,0 +1,30 @@
+package htw.berlin.ui;
+
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BurgerBotPromptProvider {
+
+    private boolean understood = true;
+
+    @Override
+    public AttributedString getPrompt() {
+        if (this.understood) {
+            return new AttributedString("BURGER-BOT:>",
+                    AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN));
+        }
+        else {
+            return new AttributedString("BURGER-BOT:>",
+                    AttributedStyle.DEFAULT.foreground(AttributedStyle.RED));
+        }
+    }
+
+    @EventListener
+    public void handle(InputEvent event) {
+        this.understood = event.getUnderstood();
+    }
+}
